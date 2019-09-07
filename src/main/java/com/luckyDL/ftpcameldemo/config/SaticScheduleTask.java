23acb9b11e61acb605fe497c57b7dca4d9c7c64d
weid <weid.wang@sunyard.com>
2019-09-07 16:01:15 +0800

    完善读取接口

---
+++ src/main/java/com/luckyDL/ftpcameldemo/config/SaticScheduleTask.java
@@ -0,0 +1,23 @@
+package com.luckyDL.ftpcameldemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author weid
+ * @create 2019-09-07 15:53
+ */
+@Configuration
+@EnableScheduling
+public class SaticScheduleTask {
+    //3.添加定时任务
+    @Scheduled(cron = "0/5 * * * * ?")
+    //或直接指定时间间隔，例如：5秒
+    //@Scheduled(fixedRate=5000)
+    private void configureTasks() {
+        System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
+    }
+}
